50472e5b906923a59465ef3ef83e8ebea3a5578c
Ann Van Eyken <ann.vaneyken@themasterlabs.com>
2018-05-13 15:03:25 +0200

    startbestanden oef heroes toegevoegd

---
+++ src/app/oefeningen/oefheroes/mock-heroes.ts
@@ -0,0 +1,3 @@
+import { Hero } from './hero-model/hero';
+
+export const HEROES: Hero[] = [new Hero(1, "Paulus de boskabouter"), new Hero(2, "Paula de Mier"), new Hero(3, "Meneer de Uil", false), new Hero(4, "Ben de Bever")];
